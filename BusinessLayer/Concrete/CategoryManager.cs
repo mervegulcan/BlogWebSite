18fc3a036ce1b0d40e9ef5b307c05ca4f92c9c15
merve aydın <gulcan.mrv@gmail.com>
2021-10-24 21:53:35 +0300

    Project Created

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EFCategoryRepository eFCategoryRepository;
+        public CategoryManager()
+        {
+            eFCategoryRepository = new EFCategoryRepository();
+        }
+
+
+       
+        public void AddCategory(Category category)
+        {
+
+            eFCategoryRepository.Insert(category);
+
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            eFCategoryRepository.Delete(category);
+
+        }
+
+        public Category GetById(int id)
+        {
+            return eFCategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return eFCategoryRepository.GetListAll();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            eFCategoryRepository.Update(category);
+        }
+    }
+}
---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal <T> where  T: class
+    {
+        List<T> GetListAll();
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        T GetById(int id);
+    }
+}
